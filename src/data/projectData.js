1953ea5e84644ba84ba291d7840bace1337479a9
Jacob Bradshaw <jakebradshaw123@gmail.com>
2021-02-28 16:47:50 -0800

    more CSS

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -4,5 +4,5 @@ export const productData = [
     { id: 3, name: "Weather Dashboard",  description: "Displays weather data for any city in the United States", href: "https://jbrad12.github.io/sturdy-parakeet/", img: "https://jbrad12.github.io/Portfolio/style.css/assets/Port2.png" },
     { id: 4, name: "Employee Directory",  description: "Employee data management system", href: "https://jbrad12.github.io/employee-directory/", img: "https://jbrad12.github.io/Portfolio/style.css/assets/Port4.png" },
     { id: 5, name: "Burger Tracker", description: "Simple SQL based burger tracker", href: "https://whispering-wildwood-11265.herokuapp.com/", img: "https://jbrad12.github.io/Portfolio/style.css/assets/Port5.png" },
-    { id: 6, name: "Party Hub",  description: "Party Hub is an application that allows a user to plan, organize and RSVP to an event", href: "", img: "https://user-images.githubusercontent.com/69474649/106051125-a47a0180-60ad-11eb-91a2-77b3df99a973.png" }
+    { id: 6, name: "Party Hub",  description: "Party Hub is an app that allows a user to plan, organize and RSVP to an event", href: "", img: "https://user-images.githubusercontent.com/69474649/106051125-a47a0180-60ad-11eb-91a2-77b3df99a973.png" }
   ]
