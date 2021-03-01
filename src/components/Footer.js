9d4913f58db5331dca966e9ab12d34ccf18636c1
Jacob Bradshaw <jakebradshaw123@gmail.com>
2021-02-28 16:04:28 -0800

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,13 @@
 export default function Footer() {
     return (
+        <div className="phantom">
         <div className="footer">
             <footer className="foot">
                 <a href="https://github.com/jbrad12" target="_blank"><i  class="fab fa-github"></i></a>
                 <a href="https://www.linkedin.com/in/jacob-bradshaw-b79008134/" target="_blank"><i class="fab fa-linkedin"></i></a>
                 <a href = "mailto: jakebradshaw123@gmail.com"><i class="fas fa-envelope-square"></i></a>
-                
             </footer>
+        </div>
         </div>
     )
 }
